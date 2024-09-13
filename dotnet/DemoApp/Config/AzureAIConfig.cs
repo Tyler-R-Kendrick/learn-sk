0ae1e4924ccabea48bdb84421921bf5c20e52289
Jason Specland <jaspecla@microsoft.com>
2024-09-13 11:04:28 -0400

    created config project

---
+++ dotnet/DemoApp/Config/AzureAIConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Config
+{
+    public class AzureAIConfig
+    {
+        public string? Endpoint { get; set; }
+        public string? Key {  get; set; }
+        public string? DeploymentName { get; set; }
+    }
+
+    public static class AzureAIConfigProvider
+    {
+        public static AzureAIConfig Load()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddUserSecrets<AzureAIConfig>()
+                .Build();
+
+            var aiConfig = new AzureAIConfig()
+            {
+                Endpoint = config["AzureAIEndpoint"],
+                DeploymentName = config["AzureAIKey"],
+                Key = config["AzureAIDeploymentName"],
+            };
+
+            return aiConfig;
+        } 
+    }
+    
+
+}
---
+++ dotnet/DemoApp/Solution1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Config;
+
+var config = AzureAIConfigProvider.Load();
+
+Console.WriteLine($"Endpoint: {config.Endpoint}");

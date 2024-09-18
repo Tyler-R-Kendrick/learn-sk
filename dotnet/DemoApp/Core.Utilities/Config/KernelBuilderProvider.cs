fedfb5bb7ff55f639b42bc3fc5c06009fe87fa6c
Pete Messina <petermessina@microsoft.com>
2024-09-18 14:26:58 -0400

    Adding history

---
+++ dotnet/DemoApp/Core.Utilities/Config/KernelBuilderProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace Core.Utilities.Config
+{
+    public static class KernelBuilderProvider
+    {
+        public static IKernelBuilder CreateKernelWithChatCompletion()
+        {
+            var applicationSettings = AISettingsProvider.GetSettings();
+
+            var builder = Kernel.CreateBuilder();
+
+            builder.AddAzureOpenAIChatCompletion(
+                applicationSettings.OpenAI.ModelName,
+                applicationSettings.OpenAI.Endpoint,
+                applicationSettings.OpenAI.Key);
+
+            return builder;
+        }
+
+    }
+}

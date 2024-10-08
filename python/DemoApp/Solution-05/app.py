36ca6199d854bd2c29af54bb843cb7e5170384ee
Frank Campise <fcampise@microsoft.com>
2024-10-08 08:27:10 -0500

    updated solution 4 to remove unnecessary assignments and added first part of solution 5, logging

--- python/DemoApp/Solution-05/app.py
+++ python/DemoApp/Solution-05/app.py
@@ -2,25 +2,32 @@ import asyncio
 import os
 import sys
 import requests
+import logging
 from datetime import datetime
 
 ### Pull in core utility libraries
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  #allows us to pull in core packages by adding the parent directory to the path
 from core.utilities.config.aiprovider import AIProvider
+from core.utilities.config.loggingprovider import LoggingProvider
 from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.functions import KernelArguments
 from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import AzureChatPromptExecutionSettings
 
+from core.utilities.filters.functioninvocationloggingfilter import FunctionInvocationLoggingFilter
+from core.utilities.filters.functioninvocationretryfilter import FunctionInvocationRetryFilter
 from core.utilities.services.mlb_service import MlbService
 from core.utilities.plugins.mlb_basebase_data import MlbBaseballData
 
 
 async def main():
     kernel = AIProvider.GetKernel()
-    kernel.add_plugin(plugin=MlbBaseballData(MlbService()),
-                      plugin_name="MlbBaseballData")
+    kernel.add_plugin(plugin=MlbBaseballData(MlbService()), plugin_name="MlbBaseballData")
     
+    logger = LoggingProvider.get_logger('learn-sk-python', logging.DEBUG)
+    kernel.add_filter("function_invocation", FunctionInvocationLoggingFilter(logger).OnFunctionInovationAsync)
+    kernel.add_filter("function_invocation", FunctionInvocationRetryFilter(max_retries=3).OnFunctionInovationAsync)
+
     execution_settings = AzureChatPromptExecutionSettings(tool_choice="auto")
     execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(auto_invoke=True)
 
@@ -32,10 +39,11 @@ async def main():
     #schedule = await mlb_service.get_team_schedule(112, datetime(2024, 9, 1), datetime(2024, 9, 30))
     #playbyplay = await mlb_service.get_game_play_by_play(744806, 40)
     
-    system_message ='''You are a sports announcer. 
-                    Summarize the game play-by-play as if you were the 
-                    famous Cubs announcer Harry Caray.'''
-
+    system_message ='''You are a sports announcer.
+        Summarize the game play-by-play as if you were the famous Cubs announcer Harry Caray.
+        When describing win or loss conditions, mention the lore/superstition that may be associated 
+        with the result."
+        '''
 
     termination_phrases = ["quit", "exit"]
     user_input = None

ef94ad2154546cdc82775bd1c134f4429ceb616f
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-10-14 16:18:03 +0000

    Added solution2 docs

--- dotnet/DemoApp/Solutions/Solution2/Program.cs
+++ dotnet/DemoApp/Solutions/Solution2/Program.cs
@@ -2,26 +2,41 @@ using Core.Utilities.Config;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
+// Load configuration.
 var builder = KernelBuilderProvider.CreateKernelWithChatCompletion();
 var kernel = builder.Build();
 
-const string terminationPhrase = "quit";
+// Execute program.
+string[] terminationPhrases = ["quit", "exit"];
 string? userInput;
 do
 {
+    // Get user input.
+    Console.WriteLine("Type 'quit' or 'exit' to terminate the program.");
     Console.Write("User > ");
-    userInput = Console.ReadLine();
+    userInput = Console.ReadLine()
+        ?.Trim().ToLowerInvariant();
 
-    OpenAIPromptExecutionSettings promptExecutionSettings = new()
+    // Validate user input.
+    while(string.IsNullOrWhiteSpace(userInput))
     {
-        ChatSystemPrompt = "You are a baseball announcer, and every time you give advice you give your advice in baseball metaphors."
-    };
+        Console.WriteLine("Please type in something for the llm to respond to.");
+        Console.Write("User > ");
+        userInput = Console.ReadLine()
+            ?.Trim().ToLowerInvariant();
+    }
 
-    KernelArguments kernelArgs = new(promptExecutionSettings);
-
-    if (userInput != null && userInput != terminationPhrase)
+    // Process assist responses.
+    if (!terminationPhrases.Contains(userInput))
     {
         Console.Write("Assistant > ");
+
+        // Create instructions for the llm to adhere to.
+        OpenAIPromptExecutionSettings promptExecutionSettings = new()
+        {
+            ChatSystemPrompt = "You are a baseball announcer, and every time you give advice you give your advice in baseball metaphors."
+        };
+        KernelArguments kernelArgs = new(promptExecutionSettings);
         await foreach (var response in kernel.InvokePromptStreamingAsync(userInput, kernelArgs))
         {
             Console.Write(response);
@@ -29,4 +44,4 @@ do
         Console.WriteLine();
     }
 }
-while (userInput != terminationPhrase);
+while (!terminationPhrases.Contains(userInput));

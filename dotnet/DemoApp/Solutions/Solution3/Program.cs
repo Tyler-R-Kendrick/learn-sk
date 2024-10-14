c76b20a3162eaa1dc2383c4344344d3c2993eeb8
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-10-14 16:31:13 +0000

    Added solution3 docs

--- dotnet/DemoApp/Solutions/Solution3/Program.cs
+++ dotnet/DemoApp/Solutions/Solution3/Program.cs
@@ -1,30 +1,51 @@
 using Core.Utilities.Config;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
+// Load configuration.
 var builder = KernelBuilderProvider.CreateKernelWithChatCompletion();
 var kernel = builder.Build();
-
-const string terminationPhrase = "quit";
-string? userInput;
-
+// Uses the OpenAI chat completions API.
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-ChatHistory chatHistory = new("You are a baseball announcer, and every time you give advice you give your advice in baseball metaphors.");
+// Insert a system prompt as instructions into the history.
+ChatHistory chatHistory = [];
 
+// Execute program.
+string[] terminationPhrases = ["quit", "exit"];
+string? userInput;
 do
 {
+    // Get user input.
+    Console.WriteLine("Type 'quit' or 'exit' to terminate the program.");
     Console.Write("User > ");
-    userInput = Console.ReadLine();
+    userInput = Console.ReadLine()
+        ?.Trim().ToLowerInvariant();
 
-    if (userInput != null && userInput != terminationPhrase)
+    // Validate user input.
+    while(string.IsNullOrWhiteSpace(userInput))
     {
-        string fullMessage = "";
+        Console.WriteLine("Please type in something for the llm to respond to.");
+        Console.Write("User > ");
+        userInput = Console.ReadLine()
+            ?.Trim().ToLowerInvariant();
+    }
+
+    //Adding the user prompt to chat history
+    chatHistory.AddUserMessage(userInput);
+
+    // Process assist responses.
+    if (!terminationPhrases.Contains(userInput))
+    {
         Console.Write("Assistant > ");
 
-        //Adding the user prompt to chat history
-        chatHistory.AddUserMessage(userInput);
-
-        await foreach (var chatUpdate in chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory))
+        string fullMessage = "";
+        // Create instructions for the llm to adhere to.
+        OpenAIPromptExecutionSettings promptExecutionSettings = new()
+        {
+            ChatSystemPrompt = "You are a baseball announcer, and every time you give advice you give your advice in baseball metaphors."
+        };
+        await foreach (var chatUpdate in chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory, promptExecutionSettings))
         {
             Console.Write(chatUpdate.Content);
             fullMessage += chatUpdate.Content ?? "";
@@ -34,4 +55,4 @@ do
         Console.WriteLine();
     }
 }
-while (userInput != terminationPhrase);
+while (!terminationPhrases.Contains(userInput));
